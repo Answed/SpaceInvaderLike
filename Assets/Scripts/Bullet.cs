5ebca5efd511da853986768877947674e82373e7
Answed <schradearwed@gmail.com>
2023-04-25 18:49:37 +0200

    Bullets are Destroyed after time

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -10,12 +10,18 @@ public class Bullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(DestroyAfterTime());
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.up * bulletSpeed * Time.deltaTime);
+    }
+
+    IEnumerator DestroyAfterTime()
+    {
+        yield return new WaitForSeconds(10);
+        Destroy(gameObject);
     }
 }

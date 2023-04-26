26a5ec45c0295b25f019575e04aa7d07446b3341
Answed <schradearwed@gmail.com>
2023-04-26 11:55:37 +0200

    Finished Base Spawn Script

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject[] enemies;
-    [SerializeField] private GameObject[] obstacles;
+    [SerializeField] private GameObject enemies;
+    [SerializeField] private GameObject obstacles;
     [SerializeField] private int spawnHeight;
     [SerializeField] private Vector2 spawnWidth; //I use vector 2 here 
     [SerializeField] private AnimationCurve enemySpawnCurve;
     [SerializeField] private AnimationCurve obstacleSpawnCurve;
+
 
     public void SpawnWave(int wave)
     {
@@ -18,13 +19,39 @@ public class WaveSpawner : MonoBehaviour
 
         for (int i = enemiesToSpawn + obstaclesToSpawn; i > 0; i--)
         {
+            var spawnEnemie = Random.Range(0, 1);
 
+            if (spawnEnemie <= 0.5f && enemiesToSpawn > 0)
+            {
+                enemiesToSpawn--;
+                SpawnObject(0);
+            }
+            else if (obstaclesToSpawn > 0)
+            {
+                obstaclesToSpawn--;
+                SpawnObject(1);
+            }
+            else if(obstaclesToSpawn == 0 && enemiesToSpawn > 0)
+            {
+                enemiesToSpawn--;
+                SpawnObject(0);
+            }
         }
     }
 
-    private void SpawnObject()
+    private void SpawnObject(int kindOfObject)
     {
+        float xSpawnValue = Random.Range(spawnWidth.x, spawnWidth.y);
+        switch (kindOfObject)
+        {
+            case 0:
+                Instantiate(enemies, new Vector2(xSpawnValue, spawnHeight), enemies.transform.rotation);
+                break;
+            case 1:
+                Instantiate(obstacles, new Vector2(xSpawnValue, spawnHeight), obstacles.transform.rotation); 
+                break; 
 
+        }
     }
 
 }

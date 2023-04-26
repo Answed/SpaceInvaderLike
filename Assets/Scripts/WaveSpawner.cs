c7f042d39b3aaa228b0039f0b2a1f90c2b0a6939
Answed <schradearwed@gmail.com>
2023-04-26 11:58:23 +0200

    Changed Difficulty scaling

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -8,14 +8,12 @@ public class WaveSpawner : MonoBehaviour
     [SerializeField] private GameObject obstacles;
     [SerializeField] private int spawnHeight;
     [SerializeField] private Vector2 spawnWidth; //I use vector 2 here 
-    [SerializeField] private AnimationCurve enemySpawnCurve;
-    [SerializeField] private AnimationCurve obstacleSpawnCurve;
 
 
     public void SpawnWave(int wave)
     {
-        int enemiesToSpawn = (int)enemySpawnCurve.Evaluate(wave);
-        int obstaclesToSpawn = (int)obstacleSpawnCurve.Evaluate(wave);
+        int enemiesToSpawn = wave * 2;
+        int obstaclesToSpawn = wave * 3;
 
         for (int i = enemiesToSpawn + obstaclesToSpawn; i > 0; i--)
         {

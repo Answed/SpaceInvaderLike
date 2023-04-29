c14b811cec3416949a459b75853a29f89d0fbbb0
Answed <schradearwed@gmail.com>
2023-04-29 15:10:30 +0200

    Added Meteor

--- Assets/Scripts/ObstacleController.cs
+++ Assets/Scripts/ObstacleController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ObstacleController : MonoBehaviour
 {
+    [SerializeField] private float speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +14,17 @@ public class ObstacleController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         
     }

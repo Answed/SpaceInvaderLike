9849b4fb0be23ccd3e429ac8cc14bedfab6b3fdb
Answed <schradearwed@gmail.com>
2023-05-01 14:17:23 +0200

    Added Score system

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    [SerializeField] private int scoreValue;
     [SerializeField] private float speed;
     [SerializeField] private int maxHealth;
     [SerializeField] private float timeBtwShots;
@@ -12,10 +13,12 @@ public class EnemyController : MonoBehaviour
     private int currentHealth;
     private float nextAttack;
 
+    private GameManager gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -34,6 +37,7 @@ public class EnemyController : MonoBehaviour
     {
         if (collision.CompareTag("Bullet"))
         {
+            gameManager.UpDateScore(scoreValue);
             Destroy(gameObject);
             Destroy(collision.gameObject);
         }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public int wave;
 
     [SerializeField] private float timeBtwWave;
+    [SerializeField] private TextMeshProUGUI scoreText;
 
     private WaveSpawner waveSpawner;
 
     private float nextWave;
+    private int score;
     public bool nextWaveSpawned;
 
     // Start is called before the first frame update
     void Start()
     {
         waveSpawner = GetComponent<WaveSpawner>();
+        scoreText.text = "Score: 0";
     }
 
     // Update is called once per frame
@@ -29,6 +33,12 @@ public class GameManager : MonoBehaviour
             nextWaveSpawned = true;
             StartCoroutine(SpawnWaveAfterTime());
         }
+    }
+    
+    public void UpDateScore(int scorePoints)
+    {
+        score += scorePoints;
+        scoreText.text = "Score: " + score;
     }
 
     IEnumerator SpawnWaveAfterTime()

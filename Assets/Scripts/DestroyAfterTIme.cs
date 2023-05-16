f3b36c4ab026e4195777812ac345a464ca6f778d
Answed <schradearwed@gmail.com>
2023-05-16 17:44:02 +0200

    Fixed DestroyAfterTimeScript

--- Assets/Scripts/DestroyAfterTIme.cs
+++ Assets/Scripts/DestroyAfterTIme.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyAfterTIme : MonoBehaviour
+public class DestroyAfterTime : MonoBehaviour
 {
     [SerializeField] private float timeBeforeDestroyed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(DestroyObjectAfterTime());
     }
-    IEnumerator DestroyAfterTime()
+    IEnumerator DestroyObjectAfterTime()
     {
         yield return new WaitForSeconds(timeBeforeDestroyed);
         Destroy(gameObject);

7100a11fdf52a1e75309d8748fee6716397ce58b
Answed <schradearwed@gmail.com>
2023-05-19 16:21:33 +0200

    Score now gets safed after each death

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -92,6 +92,8 @@ public class GameManager : MonoBehaviour
         endGame.performed += ReloadScene;
         gameIsActive = false;
         menuController.EnableGameObjects(gameOverScreenObjects);
+        int realscore = SaveData.LoadScore("score");
+        SaveData.SaveScore(realscore + score, "score");
     }
 
     private void ReloadScene(InputAction.CallbackContext context)

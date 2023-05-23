dc544a2d9523b1004fdd29e7e27feafeba17bd7c
Answed <schradearwed@gmail.com>
2023-05-23 18:59:39 +0200

    Added Upgrade Menu to main menu

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -10,11 +10,13 @@ public class MainMenuController : MenuController
     [SerializeField] private GameObject[] mainMenuButtons; // Saves all Buttons on the Main Menu -> Because there are only Buttons the name is Buttons and not Objects
     [SerializeField] private GameObject[] creditsObjects; // Saves all Credits related objects
     [SerializeField] private GameObject[] settingsObjects;
+    [SerializeField] private GameObject[] upgradeObjects;
 
     enum Window
     {
-        Credits,
-        Settings
+        Upgrade,
+        Settings,
+        Credits
     }
 
     private Window currentWindow;
@@ -29,13 +31,12 @@ public class MainMenuController : MenuController
         SceneManager.LoadScene("GameScene");
     }
 
-    public void Credits()
+    public void UpgradeShop()
     {
-        SwitchWindow(mainMenuButtons, creditsObjects);
-        titleText.text = "Credits";
-        currentWindow = Window.Credits;
+        SwitchWindow(mainMenuButtons, upgradeObjects);
+        titleText.text = "Upgrades";
+        currentWindow = Window.Upgrade;
     }
-
     public void Settings()
     {
         SwitchWindow(mainMenuButtons, settingsObjects);
@@ -43,15 +44,25 @@ public class MainMenuController : MenuController
         currentWindow = Window.Settings;
     }
 
+    public void Credits()
+    {
+        SwitchWindow(mainMenuButtons, creditsObjects);
+        titleText.text = "Credits";
+        currentWindow = Window.Credits;
+    }
+
     public void Back()
     {
         switch(currentWindow)
         {
-            case Window.Credits:
-                SwitchWindow(creditsObjects, mainMenuButtons);
+            case Window.Upgrade:
+                SwitchWindow(upgradeObjects, mainMenuButtons);
                 break;
             case Window.Settings:
                 SwitchWindow(settingsObjects, mainMenuButtons);
+                break;
+            case Window.Credits:
+                SwitchWindow(creditsObjects, mainMenuButtons);
                 break;
         }
         titleText.text = "Space Invader Like";

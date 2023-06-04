2a34e7f6ee5ab02923af331274bcb3ae88daed53
Answed <schradearwed@gmail.com>
2023-06-04 11:26:58 +0200

    Added PerkButton Struct
    Will later be used to display the current Perk in the menu

--- Assets/Scripts/PerkSelectionControler.cs
+++ Assets/Scripts/PerkSelectionControler.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class PerkSelectionControler : MonoBehaviour
 {
-    [SerializeField] private GameObject[] perkSelectorObjects;
+    private struct PerkButton
+    {
+        public Button PerkSelectionButton;
+        public TextMeshProUGUI PerkDiscription;
+    }
+
+    [SerializeField] private GameObject perkSelectorObjects;
+    [SerializeField] private PerkButton[] perkButtons;
 
     private MenuController menuController;
+
 
     // Start is called before the first frame update
     void Start()
@@ -22,11 +32,11 @@ public class PerkSelectionControler : MonoBehaviour
 
     public void OpenPerkSelector()
     {
-        menuController.EnableGameObjects(perkSelectorObjects);
+        perkSelectorObjects.SetActive(true);
     }
 
     public void ClosePerkSelector()
     {
-        menuController.DisableGameObjects(perkSelectorObjects);
+        perkSelectorObjects.SetActive(false);
     }
 }

78f651e991bfc3d0dd2c324f8585f3e802483f98
Answed <schradearwed@gmail.com>
2023-06-05 09:55:48 +0200

    Added base functions needed for the Perk System

--- Assets/Scripts/PerkSelectionControler.cs
+++ Assets/Scripts/PerkSelectionControler.cs
@@ -10,6 +10,7 @@ public class PerkSelectionControler : MonoBehaviour
     {
         public Button PerkSelectionButton;
         public TextMeshProUGUI PerkDiscription;
+        public Image PerkImage;
     }
 
     [SerializeField] private GameObject perkSelectorObjects;
@@ -39,4 +40,35 @@ public class PerkSelectionControler : MonoBehaviour
     {
         perkSelectorObjects.SetActive(false);
     }
+
+    private void LoadPerkIntoButton(PerkButton button) 
+    {
+        // Code for changing the look of a Button based on the selected Perk;
+    }
+
+    private void SelectPerkz()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            // Select one random Perk and add it to a list
+            LoadPerkIntoButton(perkButtons[i]);
+        }
+    }
+
+    #region Perk Buttons
+    public void PerkShopLeft()
+    {
+        //Applies the stats from the perk on the left or 0 in the list
+    }
+
+    public void PerkShopMiddle()
+    {
+        //Applies the stats from the perk in the middle or 1 in the list
+    }
+
+    public void PerkShopRight()
+    {
+        //Applies the stats from the perk on the right or 2 in the list
+    }
+    #endregion
 }

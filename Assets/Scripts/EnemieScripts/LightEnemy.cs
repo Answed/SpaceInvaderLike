84ecc1eb76875c2a301a7dbc78efe0d2c75eaabd
Answed <schradearwed@gmail.com>
2023-06-13 10:54:20 +0200

    Added Burst attack to light enemy

--- Assets/Scripts/EnemieScripts/LightEnemy.cs
+++ Assets/Scripts/EnemieScripts/LightEnemy.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LightEnemy : MonoBehaviour
+public class LightEnemy : EnemyController
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector2.down * enemyStats.Speed * Time.deltaTime);
+
+        if (nextAttack <= Time.time)
+        {
+            nextAttack = Time.time + enemyStats.TimeBtwShots;
+            StartCoroutine(BurstAttack());
+        }
+        if (currentHealth == 0)
+            Death();
+    }
+
+    IEnumerator BurstAttack()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(enemyStats.BulletPrefab, transform.position, enemyStats.BulletPrefab.transform.rotation);
+            yield return new WaitForSeconds(0.3f);
+        }
     }
 }

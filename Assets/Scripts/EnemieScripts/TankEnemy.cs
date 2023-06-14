3c56dba16a5effa58dcb46107dceeb461f41e657
Answed <schradearwed@gmail.com>
2023-06-14 13:07:01 +0200

    Tank Enemy now changes between staionary attacks and moving

--- Assets/Scripts/EnemieScripts/TankEnemy.cs
+++ Assets/Scripts/EnemieScripts/TankEnemy.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 
 public class TankEnemy : EnemyController
 {
+    private bool isFiring;
+
+    private void Awake()
+    {
+        nextAttack = enemyStats.TimeBtwShots * 5 + Time.time;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.down * enemyStats.Speed * Time.deltaTime);
-
-        if (nextAttack <= Time.time)
+        if(nextAttack <= Time.time && !isFiring)
         {
-            nextAttack = Time.time + enemyStats.TimeBtwShots;
-            Instantiate(enemyStats.BulletPrefab, transform.position, enemyStats.BulletPrefab.transform.rotation);
+            nextAttack += enemyStats.TimeBtwShots * 5 + Time.time;
+            StartCoroutine(Attack());
         }
-        if (currentHealth == 0)
-            Death();
+
+        if (!isFiring)
+            transform.Translate(Vector2.down * Time.deltaTime * enemyStats.Speed);
+
+    }
+
+    IEnumerator Attack()
+    {
+        isFiring = true;
+        for(int i = 0; i < 5; i++)
+        {
+            Instantiate(enemyStats.BulletPrefab, transform.position, enemyStats.BulletPrefab.transform.rotation);
+            yield return new WaitForSeconds(enemyStats.TimeBtwShots);
+        }
+        isFiring= false;
     }
 }

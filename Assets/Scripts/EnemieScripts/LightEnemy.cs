dfa1ffedfeb244445c8d01fd10c67d4b62d350aa
Answed <schradearwed@gmail.com>
2023-06-14 12:42:38 +0200

    Light Enemy moves from left to right

--- Assets/Scripts/EnemieScripts/LightEnemy.cs
+++ Assets/Scripts/EnemieScripts/LightEnemy.cs
@@ -4,18 +4,48 @@ using UnityEngine;
 
 public class LightEnemy : EnemyController
 {
+    private bool moveLeft;
+    private void Awake()
+    {
+        float distancefromLeft =transform.position.x;
+        Debug.Log(distancefromLeft);
+        if (distancefromLeft < 0)
+            moveLeft = false;
+        if (distancefromLeft > 0 )
+            moveLeft = true;
+    }
+
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.down * enemyStats.Speed * Time.deltaTime);
+
+        MoveFromSideToSide();
 
         if (nextAttack <= Time.time)
         {
             nextAttack = Time.time + enemyStats.TimeBtwShots;
             StartCoroutine(BurstAttack());
         }
+
         if (currentHealth == 0)
             Death();
+    }
+
+    private void MoveFromSideToSide()
+    {
+        if(moveLeft && transform.position.x >= -12)
+            transform.Translate(Vector2.left * enemyStats.Speed * 12 * Time.deltaTime);
+        else transform.Translate(Vector2.right * enemyStats.Speed * 12 * Time.deltaTime);
+        ChangeDirection();
+    }
+
+    private void ChangeDirection()
+    {
+        if (transform.position.x <= -11.5f)
+            moveLeft = false;
+        else if(transform.position.x >= 11.5f)
+            moveLeft = true;
     }
 
     IEnumerator BurstAttack()

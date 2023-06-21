8380d3ef166e4e88fd6511fc4a36057cce7a193a
Answed <schradearwed@gmail.com>
2023-06-21 15:15:01 +0200

    Added Audio Manager

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,17 +1,35 @@
 using UnityEngine.Audio;
+using System;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+
+    [SerializeField] private SoundClip[] soundClips;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        foreach (SoundClip clip in soundClips)
+        {
+           ApplayValuesToClip(clip);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ApplayValuesToClip(SoundClip clip)
     {
-        
+        clip.audioSource = gameObject.AddComponent<AudioSource>();
+        clip.audioSource.clip = clip.soundClip;
+
+        clip.audioSource.volume = clip.volume;
+        clip.audioSource.pitch = clip.pitch;
+        clip.audioSource.loop = clip.loop;
+    }
+
+    public void Play(string name)
+    {
+        SoundClip clip = Array.Find(soundClips, sound => sound.name == name);
+        if (clip == null)
+            return;
+        clip.audioSource.Play();
     }
 }

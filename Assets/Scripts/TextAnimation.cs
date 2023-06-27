f7885adb6fa1ec0a5b7d23b3c79f879a61ed307c
Answed <schradearwed@gmail.com>
2023-06-27 16:08:28 +0200

    Added Text AnimatorScript

---
+++ Assets/Scripts/TextAnimation.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextAnimation : MonoBehaviour
+{
+    [Range(0f, 1f)]
+    [SerializeField] private float interpolateSpeed;
+
+    private TextMeshProUGUI text;
+    private bool fadeOut;
+    private float interpolate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();  
+        fadeOut = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(fadeOut);
+        interpolate += interpolateSpeed * Time.deltaTime;
+        if (fadeOut)
+            text.color = new Color(text.color.r, text.color.g, text.color.b, Mathf.Lerp(1, 0, interpolate));
+        else
+            text.color = new Color(text.color.r, text.color.g, text.color.b, Mathf.Lerp(0, 1, interpolate));
+
+        if (text.color.a >= 1)
+        {
+            fadeOut = true;
+            interpolate = 0;
+        }
+        else if (text.color.a <= 0)
+        {
+            fadeOut = false;
+            interpolate = 0;
+        }
+    }
+}

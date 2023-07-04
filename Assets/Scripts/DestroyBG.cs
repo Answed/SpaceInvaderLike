c81af8e4b73addbed3f1805550adfb09da6581ab
Answed <schradearwed@gmail.com>
2023-07-04 15:35:47 +0200

    Added Destroy BG

---
+++ Assets/Scripts/DestroyBG.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBG : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y == -19.64f)
+            Destroy(gameObject);
+    }
+}

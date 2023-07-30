074d31613f955cd653c1618dc788323576aedb27
Answed <schradearwed@gmail.com>
2023-07-30 08:44:46 +0200

    Button now get highlighted when u hover with the mouse over it

---
+++ Assets/Scripts/ButtonFunctions.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    private AudioManager audioManager;
+    private Image background;
+
+
+    private void Start()
+    {
+        audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+        background = GetComponent<Image>();
+    }
+
+    public void OnButtonEnter()
+    {
+        audioManager.Play("HoverOverButton");
+        background.enabled = true;
+    }
+
+    public void OnButtonExit()
+    {
+        background.enabled = false;
+    }
+
+
+}
